def397120e1d2fd723b49b656749b3baebe01ec0
MehulRoy0330 <mrcodes06@gmail.com>
2021-08-22 16:42:36 +0530

    Add controls and moving background

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ function preload(){
     rocketUp = loadAnimation("imgs/rocket-up.png");
     rocketDown = loadAnimation("imgs/rocket-down.png");
     rocketStraight = loadAnimation("imgs/rocket-straight.png");
+
     meteorImg = loadImage("imgs/meteor.png");
     spaceImg = loadImage("imgs/space.png");
 }
@@ -20,6 +21,7 @@ function setup() {
     space = createSprite(width/2, height/2);
     space.addImage(spaceImg);
     space.scale = 12.0;
+    space.velocityX = -4;
 
     rocket = createSprite(100, height/2, 10, 10);
     rocket.addAnimation("straight", rocketStraight);
@@ -29,5 +31,24 @@ function setup() {
 
 function draw() {
     background(0);
+
+    if(space.x < 0){
+        space.x = width/2;
+    }
+
+    rocket.changeAnimation("straight");
+    if(keyDown("up")){
+        rocket.addAnimation("up", rocketUp);
+        rocket.changeAnimation("up");
+
+        rocket.y -= 10;
+    }
+    if(keyDown("down")){
+        rocket.addAnimation("down", rocketDown);
+        rocket.changeAnimation("down");
+
+        rocket.y +=10;
+    }
+
     drawSprites();
 }
