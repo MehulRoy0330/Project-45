23a1ff6423a5329f17ac915902725f715c132ff9
MehulRoy0330 <mrcodes06@gmail.com>
2021-08-22 17:22:59 +0530

    Design intro page

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ var rocket, rocketUp, rocketStraight, rocketDown;
 var meteor, meteorImg;
 var space, spaceImg;
 
+var gameImg;
+
 var gameState = "start";
 
 var score = 0;
@@ -13,6 +15,8 @@ function preload(){
 
     meteorImg = loadImage("imgs/meteor.png");
     spaceImg = loadImage("imgs/space.png");
+
+    gameImg = loadImage("imgs/gameImg.png");
 }
 
 function setup() {
@@ -30,7 +34,33 @@ function setup() {
 }
 
 function draw() {
-    
+    if(gameState === "start"){
+        background("cyan");
+        
+        stroke("black");
+        fill("black");
+        textSize(30);
+        text("Meteor Escape", width/2-95, height/2-200);
+
+        noStroke();
+        fill("green");
+        textSize(15);
+        text("Help the rocket dodge the meteors!!", width/2-110, height/2-150);
+
+        image(gameImg, width/2-60, height/2-120, 137, 114);
+
+        fill("red");
+        textSize(18);
+        text("Press up and down arrow keys to dodge the meteors", width/2-190, height/2+50);
+
+        fill("blue");
+        textSize(20);
+        text("Press S to start", width/2-60, height/2+150);
+
+        if(keyDown("s")){
+            gameState = "play";
+        }
+    }
     if(gameState === "play"){
         background(0);
 
@@ -65,7 +95,7 @@ function draw() {
 }
 
 function spawnMeteors(){
-    if(frameCount%90 === 0){
+    if(frameCount%60 === 0){
         meteor = createSprite(width+10, random(20, height-20), 10, 10);
         meteor.addImage(meteorImg);
         meteor.scale = random(0.2, 0.5);
