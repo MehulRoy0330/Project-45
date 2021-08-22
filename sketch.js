7481b716aa2205a989755d5404ed97e1dd69cfc4
MehulRoy0330 <mrcodes06@gmail.com>
2021-08-22 16:37:13 +0530

    Display rocket and space

--- sketch.js
+++ sketch.js
@@ -7,20 +7,27 @@ var gameState = "start";
 var score = 0;
 
 function preload(){
-  rocketUp = loadAnimation("imgs/rocket-up.png");
-  rocketDown = loadAnimation("imgs/rocket-down.png");
-  rocketStraight = loadAnimation("imgs/rocket-straight.png");
-
-  meteorImg = loadImage("imgs/meteor.png");
-
-  spaceImg = loadImage("imgs/space.png");
+    rocketUp = loadAnimation("imgs/rocket-up.png");
+    rocketDown = loadAnimation("imgs/rocket-down.png");
+    rocketStraight = loadAnimation("imgs/rocket-straight.png");
+    meteorImg = loadImage("imgs/meteor.png");
+    spaceImg = loadImage("imgs/space.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
+
+    space = createSprite(width/2, height/2);
+    space.addImage(spaceImg);
+    space.scale = 12.0;
+
+    rocket = createSprite(100, height/2, 10, 10);
+    rocket.addAnimation("straight", rocketStraight);
+    rocket.scale = 0.35;
+
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+    background(0);
+    drawSprites();
 }
